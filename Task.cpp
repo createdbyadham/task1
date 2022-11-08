ed9a65e32e6b0ef441792c239f0267ec3460f103
Adham Ehab <116720705+createdbyadham@users.noreply.github.com>
2022-11-08 23:52:28 +0200

    Add files via upload

---
+++ Task.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+class node 
+{
+    int data;
+    node *next;
+};
+class linked list 
+{
+    node * head ;
+    linked_list()
+    {
+        head = NULL ;
+    }
+    bool IsEmpty()
+    {
+       return (head == NULL);
+    }
+    void insertFirst(int i)
+    {
+     node *temp = new node();
+     temp->data = i;
+        
+        if(IsEmpty())
+        {
+            temp ->next = NULL;
+            head = temp;
+        }
+        else
+        {
+            temp ->next = head;
+            head = tamp;
+        }
+
+    }
+    void insertBefor(int item , int i)
+    {
+        if(IsFound())
+        {
+            node *temp = new node();
+            temp->data = i ;
+            node *Temp = head;
+
+        }
+        while (Temp !=NULL && Temp->new->data != item)
+        {
+           Temp = Temp->next
+        }
+        temp->next = Temp->next;
+        Temp->next = temp;
+        
+
+    }
+    else
+    {
+        cout<<"item not founded"<<endl;
+
+    }
+};
+
+int main ()
+{
+    linked_list a;
+    a.insertFirst(1);
+    a.insertFrist(2)
+    return 0;
+
+
+};
